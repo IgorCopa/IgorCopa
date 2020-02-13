95015d5c16602b8e66dc5be64121ebff6e25d235
IgorCopa <igoro.copa@gmail.com>
2020-02-13 13:39:46 -0800

    lesson 6

--- js/index.js
+++ js/index.js
@@ -2,3 +2,5 @@ const lastUpdate = document.querySelector('.lastUpdate');
 const myDate = document.lastModified;
 
 lastUpdate.innerHTML = myDate;
+
+WebFont.load({google: {families: ['Roboto', 'Acme', 'Cute Font']}});
