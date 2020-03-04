c8aff329dec32fc9f1accb2d0a66a307620087ca
IgorCopa <igoro.copa@gmail.com>
2020-03-04 10:01:02 -0800

    weather page

--- lesson9/js/prophets.js
+++ lesson9/js/prophets.js
@@ -17,8 +17,8 @@ fetch(requestURL)
             image.setAttribute('alt', `${prophets[i].name} - ${prophets[i].order}`)
 
             h2.textContent = `${prophets[i].name} ${prophets[i].lastname}`;
-            p1.textContent = `${prophets[i].birthdate}`;
-            p2.textContent = `${prophets[i].birthplace}`;
+            p1.textContent = `Date of Birth: ${prophets[i].birthdate}`;
+            p2.textContent = `Place of Birth: ${prophets[i].birthplace}`;
 
             card.appendChild(h2);
             card.appendChild(p1);
