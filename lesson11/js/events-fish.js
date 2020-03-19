c643bf797c03971c98921ea5227eec663e05c133
IgorCopa <igoro.copa@gmail.com>
2020-03-19 09:02:40 -0700

    few corrections

--- lesson11/js/events-fish.js
+++ lesson11/js/events-fish.js
@@ -10,12 +10,15 @@ fetch(requestURL)
         for (let i=0; i<towns.length; i++){
             if(towns[i].name==='Soda Springs'){
                 const events = document.querySelector('.events');
+                events.style.width = '100%';
 
-                let p1 = document.createElement('p');
-                p1.innerHTML = `${towns[i].events}`;
-                p1.style.color = 'rgb(0, 145, 0)';
+                for(let ev=0; ev<towns[i].events.length; ev++){
+                    let p1 = document.createElement('p');
+                    p1.innerHTML = towns[i].events[ev];
+                    p1.style.color = 'rgb(0, 145, 0)';
 
-                events.appendChild(p1);
+                    events.appendChild(p1);
+                }
             }
         }
     });
